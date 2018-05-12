1546138a5c6a5914fff5862d6986aa9ca875ddc9
AliHasan96 <admin@ialihasan.com>
2018-05-12 04:41:16 +0000

    Enable real time lockscreen charging current values

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -94,6 +94,9 @@ TARGET_TS_MAKEUP := true
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
 # CNE / DPM
 BOARD_USES_QCNE := true
 
